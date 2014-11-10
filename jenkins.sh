de9c66a236d8634e9e51076fc87a385b7cfbd6ff
JoyceLA <joycseleneanaid@gmail.com>
2014-11-10 08:15:13 -0600

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,17 @@
+pip install -r requirements.txt
+autopep8 -ir .
+flake8 --max-complexity=16 --exclude=*.txt,*.md --max-line-length=200 .
+cd Structured
+lettuce AceptanceTest
+cd UnitTest
+python Test_Triangle.py
+coverage run --branch Test_Triangle.py
+coverage report -m
+coverage html --title="Coverage Triangulo Estructurado - HTML"
+cd ../../Traditional
+lettuce AceptanceTest
+cd UnitTest
+python Test_Triangle.py
+coverage run --branch Test_Triangle.py
+coverage report -m
+coverage html --title="Coverage Triangulo Tradicional - HTML"
